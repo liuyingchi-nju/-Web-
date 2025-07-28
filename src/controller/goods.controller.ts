adeec60d51baf55031ecb696354dfbcdf09ec6ae
liuyingchi-nju <241250063@smail.nju.edu.cn>
2025-07-28 22:44:33 +0800

    进行部分初始数据的构建

--- src/controller/goods.controller.ts
+++ src/controller/goods.controller.ts
@@ -21,22 +21,7 @@ export class GoodsController{
 
   @Init()
   async initData(){
-    if (await this.goodsModel.count()===0) {
-      await this.goodsService.createGoods({name: "芒果TV月卡",avatarPath:"http://127.0.0.1:7001/pictures/mangotv.jpg"})
-      await this.goodsService.createGoods({name: "QQ音乐三天绿钻体验卡"})
-      await this.goodsService.createGoods({name: "bilibili大会员月卡"})
-      await this.goodsService.createGoods({name: "百度网盘不限速体验卡50张"})
-      await this.goodsService.createGoods({name: "三国杀移动版66宝珠兑换码"})
-      await this.goodsService.createGoods({name: "京东200元礼品卡"})
-      await this.goodsService.createGoods({name: "腾讯视频会员三折券"})
-      await this.goodsService.createGoods({name: "QQ音乐一天绿钻体验卡"})
-      await this.goodsService.createGoods({name: "QQ音乐绿钻月卡"})
-      await this.goodsService.createGoods({name: "原神1288原石兑换码"})
-      await this.goodsService.createGoods({name: "王者荣耀随机皮肤体验券*50"})
-      await this.goodsService.createGoods({name: "火影忍者手游神秘道具"})
-      await this.goodsService.createGoods({name: "星穹铁道神秘道具"})
-      await this.goodsService.createGoods({name: "QQ音乐绿钻月卡"})
-    }
+    await this.goodsService.initDefaultGoods();
   }
 
   @Get('')
